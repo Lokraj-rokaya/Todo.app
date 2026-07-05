cb284a0a5a58953d8610e8fa5ef7eff3f0b2f7b5
Lokraj-rokaya <lokeshrokaya08@gmail.com>
2026-07-05 09:13:39 +0545

    header refactor

--- src/BasicCard.jsx
+++ src/BasicCard.jsx
@@ -3,7 +3,7 @@ function BasicCard({name, desc}) {
   return (
     <>
       <div className="card">
-        <h1>{name}</h1>
+        <h1>Hi {name}</h1>
         <div className="desc">{desc}</div>
       </div>
     </>
